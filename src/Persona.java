7fd0ab2d6d3fc0e3fafd19cc5bdf0aace4c953f9
leandrodzn <leodzib100@gmail.com>
2021-09-27 20:38:34 -0500

    Creación clase Persona

---
+++ src/Persona.java
@@ -0,0 +1,20 @@
+public class Persona {
+    protected String nombre;
+    protected Integer edad;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public Integer getEdad() {
+        return this.edad;
+    }
+}
