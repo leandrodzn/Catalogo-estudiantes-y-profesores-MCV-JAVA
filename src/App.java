3cda9d8cdc07d17524378985e9a3bae8f6842118
leandrodzn <leodzib100@gmail.com>
2021-09-29 13:29:54 -0500

    Le escribí POO

--- src/App.java
+++ src/App.java
@@ -10,7 +10,8 @@ public class App {
         ControladorGeneral Control = new ControladorGeneral(vista);
         Control.menuPrincipal();
 
-        //Estudiante: Leandro Angel Dzib Nauat
+        //Estudiante: Leandro Angel Dzib Nauat 
+        //POO
         
 
        /* VistaEstudiante vistaEst = new VistaEstudiante();
